2693c1ae5ad2c7c1de2de0ba0ab77d7b20fef559
takayuki tanaka <takayuki.1981.0317@gmail.com>
2018-09-08 08:32:50 +0900

    [add] create asm.rs.
    アセンブラ生成部をmain.rsからasm.rsへ移動.

---
+++ src/asm.rs
@@ -0,0 +1,67 @@
+use std::process;
+use ast::Expr;
+
+/**
+ * アセンブラ生成部.
+ */
+pub struct Asm {
+    inst: String,
+}
+
+impl Asm {
+    // コンストラクタ.
+    pub fn new() -> Asm {
+        // スタート部分設定.
+        let mut start = format!(".global main\n");
+        start = format!("{}{}", start, "main:\n");
+        start = format!("{}{}", start, "  push %rbp\n");
+        start = format!("{}{}", start, "  mov %rsp, %rbp\n");
+
+        Asm { inst: start }
+    }
+
+    // アセンブラ取得
+    pub fn get_inst(&self) -> String {
+        // 終了部分を結合し、返却.
+        let mut end = format!("  movl 0(%rsp), %eax\n");
+        end = format!("{}{}", end, "  add $4, %rsp\n");
+        end = format!("{}{}", end, "  pop %rbp\n");
+        end = format!("{}{}", end, "  ret\n");
+        format!("{}{}", self.inst, end)
+    }
+
+    // アセンブラ生成.
+    pub fn generate(&mut self, ast: &Expr) {
+        match *ast {
+            Expr::Plus(ref a, ref b) |
+            Expr::Minus(ref a, ref b) |
+            Expr::Multiple(ref a, ref b) => {
+                self.generate(a);
+                self.generate(b);
+
+                // 各演算子評価.
+                self.inst = format!("{}{}", self.inst, "  movl 0(%rsp), %edx\n  add $4, %rsp\n");
+                self.inst = format!("{}{}", self.inst, "  movl 0(%rsp), %eax\n  add $4, %rsp\n");
+                self.inst = format!("{}{}", self.inst, self.operator(ast));
+
+                // 演算結果をrspへ退避.
+                self.inst = format!("{}{}", self.inst, "  sub $4, %rsp\n  movl %eax, 0(%rsp)\n");
+            }
+            Expr::Factor(a) => {
+                // 数値.
+                self.inst = format!("{}{}", self.inst, "  sub $4, %rsp\n");
+                self.inst = format!("{}  movl ${}, 0(%rsp)\n", self.inst, a);
+            }
+        }
+    }
+
+    // 演算子アセンブラ生成.
+    fn operator(&self, ope: &Expr) -> String {
+        match *ope {
+            Expr::Multiple(_, _) => "  mull %edx\n".to_string(),
+            Expr::Plus(_, _)     => "  addl %edx, %eax\n".to_string(),
+            Expr::Minus(_, _)    => "  subl %edx, %eax\n".to_string(),
+            _ => process::abort()
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,15 @@
 mod lexer;
 mod token;
 mod ast;
+mod asm;
 
-use std::process;
 use ast::Ast;
-use ast::Expr;
-
-/**
- * 演算子をアセンブラへ.
- */
-fn operator(ope: &Expr) {
-    match *ope {
-        Expr::Multiple(_, _) => println!("  mull %edx"),
-        Expr::Plus(_, _)     => println!("  addl %edx, %eax"),
-        Expr::Minus(_, _)    => println!("  subl %edx, %eax"),
-        _ => process::abort()
-    }
-}
-
-/**
- * 式評価.
- */
-fn expression(ast: &Expr) {
-    match *ast {
-        Expr::Plus(ref a, ref b) |
-        Expr::Minus(ref a, ref b) |
-        Expr::Multiple(ref a, ref b) => {
-            expression(a);
-            expression(b);
-
-            // 各演算子評価.
-            println!("  movl 0(%rsp), %edx\n  add $4, %rsp");
-            println!("  movl 0(%rsp), %eax\n  add $4, %rsp");
-            operator(ast);
-
-            // 演算結果をrspへ退避.
-            println!("  sub $4, %rsp\n  movl %eax, 0(%rsp)");
-        }
-        Expr::Factor(a) => {
-            // 数値.
-            println!("  sub $4, %rsp");
-            println!("  movl ${}, 0(%rsp)", a);
-        }
-    }
-}
+use asm::Asm;
 
 /**
  * メイン関数.
  */
 fn main() {
-    println!(".global main");
-    println!("main:");
-    println!("  push %rbp");
-    println!("  mov %rsp, %rbp");
-
     // 標準入力を字句解析
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).unwrap();
@@ -61,11 +17,8 @@ fn main() {
     p.read_token();
     let mut ast = Ast::new(p.get_tokens());
 
-    // 演算実施.
-    expression(&ast.parse());
-
-    println!("  movl 0(%rsp), %eax");
-    println!("  add $4, %rsp");
-    println!("  pop %rbp");
-    println!("  ret");
+    // アセンブラへ変換.
+    let mut asm = Asm::new();
+    asm.generate(&ast.parse());
+    println!("{}", asm.get_inst());
 }
