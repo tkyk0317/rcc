26033390755b74d25268c8419d50ad902d78510b
takayuki tanaka <takayuki.1981.0317@gmail.com>
2018-12-11 12:34:50 +0900

    improve semiantic.rs

--- src/semantic.rs
+++ src/semantic.rs
@@ -20,35 +20,37 @@ impl<'a> Semantic<'a> {
 
     // 解析開始
     pub fn exec(&self) -> Result<(), Vec<String>> {
-        let errs = self.ast.get_tree().iter().fold(vec![], |mut acc, t| {
+        let errs = self.ast.get_tree().iter().fold(Vec::<String>::new(), |mut init, t| {
             match self.analysis(&t) {
-                Err(r) => {
-                    acc.push(r);
-                    acc
+                Err(ref mut r) => {
+                    init.append(r);
+                    init
                 }
-                Ok(_) => acc,
+                Ok(_) => init,
             }
         });
         if errs.is_empty() { Ok(()) } else { Err(errs) }
     }
 
     // 解析
-    fn analysis(&self, ast: &AstType) -> Result<(), String> {
+    fn analysis(&self, ast: &AstType) -> Result<(), Vec<String>> {
         match ast {
-            AstType::FuncDef(ref t, ref name, ref args, ref stmt) => {
-                match t {
-                    Type::Unknown(n) => Err(format!("Cannot found Type: {:?}", n)),
-                    _ => {
-                        if self.funcs.search(name).is_none() {
-                            Err(format!("Cannot found function name: {}", name))
-                        } else {
-                            Ok(())
-                        }
-                    }
-                }
-            },
+            AstType::FuncDef(ref t, ref n, ref a, ref s) => self.analysis_funcdef(t, n, a, s),
             _ => Ok(())
         }
+    }
+
+    // 関数定義解析
+    fn analysis_funcdef(&self, t: &Type, name: &String, args: &AstType, stmt: &AstType) -> Result<(), Vec<String>> {
+        let mut errs = Vec::<String>::new();
+        match t {
+            Type::Unknown(n) => errs.push(format!("Cannot found Type: {:?}", n)),
+            _ => {},
+        }
+        if self.funcs.search(name).is_none() {
+            errs.push(format!("Cannot found function name: {}", name));
+        }
+        if errs.is_empty() { Ok(())} else { Err(errs) }
     }
 }
 
@@ -95,6 +97,7 @@ fn test_func_type() {
         let vars = SymbolTable::new();
         let r = Semantic::new(&tree, &vars, &funcs).exec();
         assert!(r.is_err());
+        assert!(r.err().unwrap().len() == 1);
     }
     // 関数が登録されていない
     {
@@ -115,5 +118,27 @@ fn test_func_type() {
         let vars = SymbolTable::new();
         let r = Semantic::new(&tree, &vars, &funcs).exec();
         assert!(r.is_err());
+        assert!(r.err().unwrap().len() == 1);
+    }
+    // 関数が定義されていないかつ、型がおかしい
+    {
+        let ast = vec![
+            AstType::FuncDef(
+                Type::Unknown("aaaa".to_string()),
+                "main".to_string(),
+                Box::new(AstType::Argment(vec![])),
+                Box::new(AstType::Statement(vec![
+                    AstType::Return(Box::new(
+                        AstType::Variable(Type::Int, "".to_string())
+                    ))
+                ]))
+            )
+        ];
+        let tree = AstTree { tree: ast };
+        let funcs = SymbolTable::new();
+        let vars = SymbolTable::new();
+        let r = Semantic::new(&tree, &vars, &funcs).exec();
+        assert!(r.is_err());
+        assert!(r.err().unwrap().len() == 2);
     }
 }
